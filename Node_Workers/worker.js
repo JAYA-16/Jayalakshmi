48b410bedf4ff47335d8a622f67dc5f09fa14b00
JAYA-16 <59077978+JAYA-16@users.noreply.github.com>
2020-04-22 21:54:39 +0530

    Delete worker.js

--- Node_Workers/worker.js
+++
@@ -1,4 +0,0 @@
-const {workerData,parentPort} = require('worker_threads');
-console.log("This is the data in the worker---->", workerData.value)
-
-parentPort.postMessage("AWESOME!!!! GO AHEAD....");
